160f6692716f74209efa03eb819bae10ab225011
dennis <king@hiberniagaming.net>
2016-03-16 03:15:36 -0400

    Initial commit

---
+++ DummyEveLauncher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace DummyEveLauncher
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("Launcher\\evelauncher.exe", String.Join(" ", args));
+        }
+    }
+}
